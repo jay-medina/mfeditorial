2759976766a3ff809de9fb09a19399465f403b8c
Jose Medina <jomedina@microsoft.com>
2016-04-07 15:21:32 -0700

    Adding flag for production compile of react

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
       ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ]
 };
