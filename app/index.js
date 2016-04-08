be223ff8e42a0203144011db6985d890b3744649
Jose Medina <jomedina@microsoft.com>
2016-04-08 14:26:51 -0700

    finishing the react-tutorial

--- app/index.js
+++ app/index.js
@@ -3,6 +3,11 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const data = [
+  {id: 1, author: "Jose Medina", text: "This is one comment"},
+  {id: 2, author: "Mercedes Fernandez", text: "This is another comment"}
+];
+
 const Comment = React.createClass({
   render() {
     return (
@@ -25,40 +30,111 @@ const CommentList = React.createClass({
     );
   },
   renderComments() {
-    const data = this.props.data;
-    return data.map((comment) => <Comment key={comment.id} author={comment.author}>{comment.text}</Comment>);
+    return this.props.data.map(comment => (
+                <Comment key={comment.id} author={comment.author}>
+                  {comment.text}
+                </Comment>
+            ));
   }
 });
 
 const CommentForm = React.createClass({
+  getInitialState() {
+    return {
+      author: '',
+      text: ''
+    };
+  },
   render() {
     return (
-      <div className="commentForm">
-        Hello World! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange} />
+
+        <input type="text" placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleCommentChange} />
+
+        <input type="submit" value="Post"/>
+      </form>
     );
+  },
+  handleAuthorChange(e) {
+    this.setState({author: e.target.value});
+  },
+  handleCommentChange(e){
+    this.setState({text: e.target.value});
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    let {author, text} = this.state;
+    author = author.trim();
+    text = text.trim();
+
+    if(text && author) {
+      this.props.onCommentSubmit({author, text});
+      this.setState({author: '', text: ''});
+    }
   }
 });
 
 const CommentBox = React.createClass({
+  getInitialState() {
+    return {
+      data: []
+    };
+  },
+  componentDidMount() {
+    const retrieveDataPromise = new Promise(resolve => {
+      setTimeout(function() {
+        resolve(data);
+      }, 2000);
+    });
+
+    retrieveDataPromise.then(result => this.setState({data: result}));
+  },
   render() {
+    if(this.isDataFulfilled()){
+      return this.renderComments();
+    }
+    else {
+      return this.renderLoading();
+    }
+  },
+  isDataFulfilled() {
+    return this.state.data.length > 0;
+  },
+  renderLoading() {
+    return <h3>Loading</h3>;
+  },
+  renderComments() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
     );
+  },
+  handleCommentSubmit: function(comment) {
+    //refresh the list
+    const oldData = this.state.data;
+    const highestId = oldData.map(x => x.id)
+                             .reduce((total, next) => (total > next)? total : next, 0);
+
+    const newData = oldData.concat([
+                      {id: highestId + 1,
+                       author: comment.author,
+                       text: comment.text}
+                     ]);
+    this.setState({data: newData});
   }
 });
 
-const data = [
-  {id: 1, author: "Jose Medina", text: "This is one comment"},
-  {id: 2, author: "Mercedes Fernandez", text: "This is another comment"}
-];
-
 ReactDOM.render(
-  <CommentBox data={data}/>,
+  <CommentBox />,
   document.getElementById('app')
 );
 
