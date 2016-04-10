e8a8e0e621713f41aefd8eb0e66c597968638b19
Jose Medina <josemedina921@gmail.com>
2016-04-10 16:06:44 -0700

    Adding Footer info

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,10 +1,15 @@
+import './Footer.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Footer = React.createClass({
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="footer">
+        Created By <a target="_blank" href="https://twitter.com/mrfunkycold">@MrFunkyCold</a>
+      </div>
+    );
   }
 });
 
