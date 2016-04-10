ab8aa929522388f5b9643925300d02260b5212a8
Jose Medina <josemedina921@gmail.com>
2016-04-10 15:52:52 -0700

    Contact info

--- app/Contact.jsx
+++ app/Contact.jsx
@@ -3,18 +3,36 @@ import './Contact.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ContactContainer = React.createClass({
+  render() {
+    const {title, children} = this.props;
+
+    return (
+    <div className={this._getClassName()}>
+      <header className="contactInfo__header">{title}</header>
+      <section>{children}</section>
+    </div>
+    );
+  },
+  _getClassName() {
+    return "container " + this.props.className;
+  }
+});
+
 const Contact = React.createClass({
   render() {
     return (
       <div className="contactInfo">
-        <div className="container email">
-          <header className="contactInfo__header">Contact me</header>
-          <section>If you'd like to get in touch, please email: temp@gmail.com</section>
-        </div>
-        <div className="container socialMedia">
-          <header className="contactInfo__header">Social</header>
-          <section>Twitter, Facebook, Instagram, LinkedIn</section>
-        </div>
+        <ContactContainer className="email" title="Contact Me">
+          If you'd like to get in touch, please email: temp@gmail.com
+        </ContactContainer>
+        <ContactContainer className="socialMedia" title="Social">
+          Twitter, Facebook, Instagram, LinkedIn
+        </ContactContainer>
+        <ContactContainer className="portfolio" title="Portfolio">
+          Book with a naked guy in front. Book with a naked guy in front.
+          Book with a naked guy in front. Book with a naked guy in front.
+        </ContactContainer>
       </div>);
   }
 });
