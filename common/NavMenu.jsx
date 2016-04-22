cea586515a593d1dae954f95f21c4587c479d75f
Jose Medina <josemedina921@gmail.com>
2016-04-21 23:57:09 -0700

    Updates

--- common/NavMenu.jsx
+++ common/NavMenu.jsx
@@ -8,12 +8,12 @@ const NavMenu = React.createClass({
   render() {
     return (
       <ul className="navMenu">
-        <li className="menuItem"><NavLink to="/">Home</NavLink></li>
-        <li className="menuItem"><NavLink to="/about">About</NavLink></li>
+        <li className="menuItem"><NavLink to="./">Home</NavLink></li>
+        <li className="menuItem"><NavLink to="./about">About</NavLink></li>
         <li className="menuItem"><NavLink to="#">Portfolio</NavLink></li>
         <li className="menuItem"><NavLink to="#">Services and Rates</NavLink></li>
         <li className="menuItem"><NavLink to="#">FAQ</NavLink></li>
-        <li className="menuItem"><NavLink to="/contact">Contact</NavLink></li>
+        <li className="menuItem"><NavLink to="./contact">Contact</NavLink></li>
       </ul>
     );
   }
